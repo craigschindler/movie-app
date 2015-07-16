33aa23fc70ef502be63f63f9dfa7ddeddeeef639
Ubuntu <ubuntu@ip-172-31-42-191.us-west-2.compute.internal>
2015-07-16 19:36:50 +0000

    First commit

---
+++ client/app/scripts/controllers/movies.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name clientApp.controller:MoviesCtrl
+ * @description
+ * # MoviesCtrl
+ * Controller of the clientApp
+ */
+angular.module('clientApp')
+  .controller('MoviesCtrl', ['$scope', 'Movie', function ($scope, Movie) {
+      $scope.movies = Movie.getList().$object;
+  }]);
